fc3648ce312cad0b3c75b48d11523e3efd6394e3
Elan Qisthi <elan.qisthi@bukalapak.com>
2018-05-12 01:46:33 +0700

    BMI app

---
+++ BMI cal/BMI cal/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  BMI cal
+//
+//  Created by Elan Qisthi on 12/05/18.
+//  Copyright © 2018 Elan Qisthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var bodyWeightLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var bodyWeightOutlet: UITextField!
+    @IBOutlet weak var bodyHeightOutlet: UITextField!
+    
+    @IBAction func bodyWeightEdit(_ sender: UITextField) {
+        calculateBMI()
+    }
+    
+    @IBAction func bodyHeightEdit(_ sender: UITextField) {
+        calculateBMI()
+    }
+    
+    func calculateBMI() {
+        let height = Float(bodyWeightOutlet.text ?? "") ?? 1
+        let weight = Float(bodyHeightOutlet.text ?? "") ?? 0
+        let bmi = weight / ((height/100)*(height/100))
+        
+        print(height)
+        print(weight)
+        print(bmi)
+        if bmi != 0 {
+            descriptionLabel.text = getBMIDescription(bmi: bmi)
+            bodyWeightLabel.text = "\(bmi) %"
+        }
+        
+    }
+    
+    func getBMIDescription(bmi: Float) -> String {
+        if bmi > 25.0 {
+            return "Overweight"
+        }
+        
+        if bmi < 18.5 {
+            return "Underweight"
+        }
+        
+        return "normal"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //dismiss keyboard
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+            view.endEditing(true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
